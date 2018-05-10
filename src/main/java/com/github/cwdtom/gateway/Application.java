58e568460c3a3b04f55e5360196f62a87586b3f4
chen weidong <cwdtom@gmail.com>
2018-05-10 14:41:25 +0800

    begin

---
+++ src/main/java/com/github/cwdtom/gateway/Application.java
@@ -0,0 +1,13 @@
+package com.github.cwdtom.gateway;
+
+/**
+ * 启动类
+ *
+ * @author chenweidong
+ * @since 1.0.0
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
