776c7f3bd19d948bc0da2344c4c46319f9d21b71
chen weidong <cwdtom@gmail.com>
2018-05-24 15:07:09 +0800

    update version

--- src/main/java/com/github/cwdtom/gateway/entity/Constant.java
+++ src/main/java/com/github/cwdtom/gateway/entity/Constant.java
@@ -10,7 +10,7 @@ public class Constant {
     /**
      * 版本号
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     /**
      * 最大单个请求大小，10MB
      */
